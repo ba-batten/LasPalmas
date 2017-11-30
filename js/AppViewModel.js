69425b31b8c44d43aeba73dd59ca5eadfc6872c9
Brandon Batten <ba_batten@live.com>
2017-11-30 11:55:03 -0500

    Consolidate jQuery .css()

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -58,23 +58,19 @@ function AppViewModel() {
 
   // Change wordmark container to be a bar across top of page
   self.changeWordmarkContainer = function() {
-    $('.m-wordmark-container').css('width', '100%');
-    $('.m-wordmark-container').css('clip-path',
-     'polygon(0% 0%, 100% 0%, 100% 100%, 50% 100%, 0% 100%)');
-    $('.m-wordmark-container').css('-webkit-clip-path',
-    'polygon(0% 0%, 100% 0%, 100% 100%, 50% 100%, 0% 100%)');
-    $('.m-wordmark-container').css('padding', '10px 0');
+    $('.m-wordmark-container').css({'width': '100%',
+                                    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 100%, 50% 100%, 0% 100%)',
+                                    '-webkit-clip-path': 'polygon(0% 0%, 100% 0%, 100% 100%, 50% 100%, 0% 100%),
+                                    'padding': '10px 0'});
     $('.t-tagline').css('display', 'none');
   }
 
   // Reset wordmark container to be a shape centered at top of page
   self.resetWordmarkContainer = function() {
-    $('.m-wordmark-container').css('width', '250px');
-    $('.m-wordmark-container').css('clip-path',
-     'polygon(0% 0%, 100% 0%, 97% 80%, 50% 100%, 3% 80%)');
-    $('.m-wordmark-container').css('-webkit-clip-path',
-     'polygon(0% 0%, 100% 0%, 97% 80%, 50% 100%, 3% 80%)');
-    $('.m-wordmark-container').css('padding', '10px 15px 20px');
+    $('.m-wordmark-container').css({'width': '250px', 
+                                    'clip-path': 'polygon(0% 0%, 100% 0%, 97% 80%, 50% 100%, 3% 80%)',
+                                    '-webkit-clip-path': 'polygon(0% 0%, 100% 0%, 97% 80%, 50% 100%, 3% 80%)',
+                                    'padding': '10px 15px 20px'});
     $('.t-tagline').css('display', 'block');
 
   }
