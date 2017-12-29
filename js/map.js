17a80719ba4ffe23c31fd69dfb24f1fa5120fa6e
Brandon Batten <brandon@brandon.dev.machine>
2017-12-28 22:20:26 -0500

    Add a loop that creates a marker at each location

--- js/map.js
+++ js/map.js
@@ -4,8 +4,21 @@ function initMap() {
     center: {lat: 35.94, lng: -78.77},
     zoom: 10
   });
-}
+
+  //
+  // Add markers
+  //
+  model.data.locations.locs.forEach(function(location) {
+    var marker = new google.maps.Marker({
+          position: {lat: 35.94, lng: -78.77},
+          map: map,
+          title: location.description
+        });
+        console.log(marker);
+    });
+
+};
 
 function mapsError() {
   alert("Map unavailable")
-}
+};
